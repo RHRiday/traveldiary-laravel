bda3956009c07cdf8398e8a23260fabef739d45d
RHRiday <riffatriday@gmail.com>
2021-07-25 23:55:22 +0600

    last change

--- public/js/main.js
+++ public/js/main.js
@@ -95,3 +95,20 @@ $(document).ready(function() {
         }
     });
 });
+
+
+// places
+
+$(document).ready(function () {
+    $("input[type='radio']").click(function () {
+      var sim = $("input[type='radio']:checked").val();
+      //alert(sim);
+      if (sim < 3) {
+        $(".myratings").css("color", "red");
+        $(".myratings").text(sim);
+      } else {
+        $(".myratings").css("color", "green");
+        $(".myratings").text(sim);
+      }
+    });
+  });
