c1f313e9ee84bfbc0e9ef1f6d5e8f6d47be8e74c
shajjadkabir <sajjadkabir71@gmail.com>
2021-08-02 22:01:22 +0600

    places dropdown menu added

--- resources/views/place/index.blade.php
+++ resources/views/place/index.blade.php
@@ -13,6 +13,61 @@
 
 @include('partials.nav')
 
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+    
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                  </li>
+    
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        বাংলাদেশ ভ্রমণ
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a class="dropdown-item" href="#">Dhaka</a>
+                      <a class="dropdown-item" href="#">Chittagong</a>
+                      <a class="dropdown-item" href="#">Barishal</a>
+                      <a class="dropdown-item" href="#">Rajshahi</a>
+                      <a class="dropdown-item" href="#">Sylhet</a>
+                    </div>
+                  </li>
+    
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        জনপ্রিয় স্থান
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a class="dropdown-item" href="#">Cox's Bazar</a>
+                      <a class="dropdown-item" href="#">Saint Martin</a>
+                      <a class="dropdown-item" href="#">Tanguar Haor</a>
+                    </div>
+                  </li>
+    
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        যোগাযোগ  
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Advertisement</a>
+                      
+                    </div>
+                  </li>
+              </ul>
+            </div>
+        </div>
+          </nav>
+
 <div class="container">
 
     <div class="row align-items-center places mt-5">
